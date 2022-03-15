3c513ab2fa5bb805c8dd100442ec46583dba5b4f
Zerui An <anzerui@126.com>
2022-03-15 16:48:54 -0500

    Revert to previous MainVievModel

--- controller/Android/MyController/app/src/main/java/com/example/mycontroller/MainViewModel.java
+++ controller/Android/MyController/app/src/main/java/com/example/mycontroller/MainViewModel.java
@@ -1,12 +1,7 @@
 package com.example.mycontroller;
 
-import android.content.Context;
-
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
     public final MyBluetooth myBluetooth = new MyBluetooth();
-    private MutableLiveData<Integer> myBluetoothStatus = new MutableLiveData<Integer>();
 }

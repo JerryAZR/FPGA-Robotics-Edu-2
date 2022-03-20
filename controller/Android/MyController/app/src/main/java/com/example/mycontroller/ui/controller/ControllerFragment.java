cd5d1b6cf05363ab77699ee66790e61dcbff913a
Zerui An <anzerui@126.com>
2022-03-19 20:24:19 -0500

    Add virtual joysticks

--- controller/Android/MyController/app/src/main/java/com/example/mycontroller/ui/controller/ControllerFragment.java
+++ controller/Android/MyController/app/src/main/java/com/example/mycontroller/ui/controller/ControllerFragment.java
@@ -14,15 +14,49 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mycontroller.databinding.FragmentControllerBinding;
 
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
 public class ControllerFragment extends Fragment {
 
     private FragmentControllerBinding binding;
+    private String left_info = "Left: +0";
+    private String right_info = "; Right: +0";
+    private int left_reading = 0;
+    private int right_reading = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentControllerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.joystickLeft.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                if (angle < 180) {
+                    left_info = "Left: +" + strength;
+                    left_reading = strength;
+                } else {
+                    left_info = "Left: -" + strength;
+                    left_reading = -strength;
+                }
+                binding.textController.setText(left_info + right_info);
+            }
+        });
+
+        binding.joystickRight.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                if (angle < 90) {
+                    right_info = "; Right: +" + strength;
+                    right_reading = strength;
+                } else {
+                    right_info = "; Right: -" + strength;
+                    right_reading = -strength;
+                }
+                binding.textController.setText(left_info + right_info);
+            }
+        });
 
         return root;
     }
